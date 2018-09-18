89e84f0af206cfe263b3f0598520b82b7235d925
Vadim <galyamoff@hotmail.com>
2018-09-18 10:09:01 +0300

    point 1: completeness is finished

--- src/component/Completeness.js
+++ src/component/Completeness.js
@@ -1,24 +1,55 @@
+import React from "react";
 import {Component} from "react";
+import axios from "axios";
 
 class Completeness extends Component {
     constructor(props){
         super(props);
+        this.state = {value: 'Комплектность', completenessIsFull : props.isFull};
         this.handleChange = this.handleChange.bind(this);
+
     }
 
-    handleChange() {
-        this.setState({value: event.target.value});
+    handleChange(event) {
+
+            this.setState({value: event.target.value});
+            if(event.target.value === 'Full' || event.target.value === 'IsNotFull') {
+                axios.get(sessionStorage.getItem('URL') + '?func=nnsodwebui.setincomplete&Id=' + sessionStorage.getItem("ID") + '&isIncomplete=' + event.target.value)
+
+                    .then((response) => {
+                        if(response.data.errMsg.length > 0){
+                            alert (response.data.errMsg)
+                        }
+                        else {
+                            this.setState({completenessIsFull: response.data.completenessIsFull});
+                        }
+                        this.setState({value: "Комплектность"});
+
+                    })
+
+                    .catch(error => {
+                        alert(error);
+                        this.setState({value: "Комплектность"})
+                    });
+            }
     }
 
     render() {
         return (
+            <React.Fragment>
+                <select className={"nnsod-actions-neutral"}  value={this.state.value} onChange={this.handleChange}>
+                    <option key="1" value="Completeness">Комплектность</option>
+                    <option key="2" value="Full">Полный комлект</option>
+                    <option key="3" value="IsNotFull">Неполный комплект</option>
+                 </select>
+                <div className={"completeness"}>
+                   <p className={'nnsodIsComplete'}>{this.state.completenessIsFull ? 'Полный комплект' : 'Неполный комплект'}</p>
+                </div>
+            </React.Fragment>
 
-            <select className={"nnsod-actions-neutral"}  value={this.state.value} onChange={this.handleChange}>
-                <option value="grapefruit">Grapefruit</option>
-                <option value="lime">Lime</option>
-                <option value="coconut">Coconut</option>
-                <option value="mango">Mango</option>
-            </select>
+
         )
     }
 }
+
+export default Completeness
--- src/component/RenderButtons.js
+++ src/component/RenderButtons.js
@@ -1,249 +1,36 @@
 import React from "react";
 import axios from "axios";
+import Completeness from './Completeness'
 
 
 class RenderButtons extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            res: []
+            res: ""
         }
     }
 
     componentDidMount(){
         axios({
             method:'get',
-            url:'https://reqres.in/api/products/3',
+            url:'https://sms01110.npr.nornick.ru/OTCS/livelink.exe?func=nnsodwebui.displaybuttons'
 
         })
             .then((response) => {
-                const res = response.data;
-                this.setState({ res });
-                console.log(this.state.res.data.color)
+                const isFull = response.data.completeness.isFull;
+                const show = response.data.completeness.show;
+                this.setState({ isFull, show });
             });
 
     }
 
-    handlePaymentClick() {
-
-    }
-
-    handleProcessingSapHCMClick() {
-
-    }
-
-    handleLinkDocumentClick() {
-
-    }
-
-    handleUnlinkDocumentClick() {
-
-    }
-
-    handleCopyKeyLinkClick(){
-
-    }
-
-    handleCreateDuplicateClick(){
-
-    }
-
-    handleCreateCommentClick(){
-
-    }
-
-    handleGenerateRegistryClick(){
-
-    }
-
-    handleReturnToIncomingClick(){
-
-    }
-
-    handleAddPersonalNumbersListClick(){
-
-    }
-
-    handleSendForReviewClick(){
-
-    }
-
-    handleDeleteClick(){
-
-    }
-
-    handlePickForProcessingClick(){
-
-    }
-
-    handleSetPerformerClick(){
-
-    }
-
-    handlePrintCorrectionSheetClick(){
-
-    }
-
-    handlePrintContactSheetClick(){
-
-    }
-
-    handlePublishClick(){
-
-    }
-
-    handleAcceptOriginalsClick(){
-
-    }
-
-    handleBankStatementCardRegistryLinkClick(){
-
-    }
-
-    handleSuspendClick(){
-
-    }
-
-    handleReturnToWorkClick(){
-
-    }
-
-    handleRefuseClick(){
-
-    }
-
-    handleLinkedClick(){
-
-    }
-
-    handlePrintClick(){
-
-    }
-
-    handleFillingStorageAttributesClick(){
-
-    }
-
-    handleReceivedOriginalsClick(){
-
-    }
-
-    handleShowXRefConnectClick(){
-
-    }
-    //<!-- todo:: move handler to component -->
 
     render() {
-        const data = this.state.res.data;
+
         return (
             <div>
-                {data.completeness.show &&
-                    <Completeness />
-                }
-
-                {data.payment.show &&
-                    <Payment />
-                }
-
-                {data.processingsaphcm.show &&
-                    <ProcessingSAPHCM />
-                }
-
-                {data.linkdocument.show &&
-                    <LinkDocument />
-                }
-
-                {data.unlinkdocument.show &&
-                    <UnlinkDocument />
-                }
-
-                {data.copykeylink.show &&
-                    <CopyKeyLink />
-                }
-
-                {data.createduplicate.show &&
-                    <CreateDuplicate />
-                }
-
-                {data.createcomment.show &&
-                    <CreateComment />
-                }
-
-                {data.generateregistry.show &&
-                    <GenerateRegistry />
-                }
-
-                {data.returntoincoming.show &&
-                    <ReturnToIncoming />
-                }
-
-                {data.addpersonalnumberslist.show &&
-                    <AddPersonalNumbersList />
-                }
-
-                {data.sendforreview.show &&
-                    <SendForReview />
-                }
-
-                {data.delete.show &&
-                    <Delete />
-                }
-
-                {data.pickforprocessing.show &&
-                    <PickForProcessing />
-                }
-
-                {data.setperformer.show &&
-                <SetPerformer />
-                }
-
-                {data.printcorrectionsheet.show &&
-                <PrintCorrectionSheet />
-                }
-
-                {data.publish.show &&
-                <Publish />
-                }
-
-                {data.acceptoriginals.show &&
-                <AcceptOriginals />
-                }
-
-                {data.bankstatementcardregistrylink.show &&
-                <BankStatementCardRegistryLink />
-                }
-
-                {data.suspend.show &&
-                <Suspend />
-                }
-
-                {data.returntowork.show &&
-                <ReturnToWork />
-                }
-
-                {data.refuse.show &&
-                <Refuse />
-                }
-
-                {data.linked.show &&
-                <Linked />
-                }
-
-                {data.print.show &&
-                <Print />
-                }
-
-                {data.fillingstorageattributes.show &&
-                <FillingStorageAttributes />
-                }
-
-                {data.recievedoriginals.show &&
-                <RecievedOriginals />
-                }
-
-                {data.showxrefconnect.show &&
-                <ShowXRefConnect />
-                }
+                <Completeness isFull={this.state.isFull} show = {this.state.show}/>
             </div>
         )
     }
